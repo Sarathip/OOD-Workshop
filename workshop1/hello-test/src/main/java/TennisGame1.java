66b7f800749d82032476b1f2b6da9e9baffb84e1
sarathip.tec <sarathip.tec@ngernturbo.com>
2021-07-24 09:15:07 +0700

    refactor

--- workshop1/hello-test/src/main/java/TennisGame1.java
+++ workshop1/hello-test/src/main/java/TennisGame1.java
@@ -1,10 +1,11 @@
+import java.util.concurrent.atomic.AtomicReference;
+
 public class TennisGame1 {
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name = "";
     private String player2Name = "";
     private String[] scoreResults = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-
     public TennisGame1(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
@@ -26,12 +27,12 @@ public class TennisGame1 {
     }
 
     private String GetScoreMoreThanFour() {
-        String score = "";
+        AtomicReference<String> score = new AtomicReference<>("");
         int minusResult = m_score1-m_score2;
-        if (minusResult == 1) score ="Advantage " + player1Name;
-        else if (minusResult ==-1) score ="Advantage " + player2Name;
-        else if (minusResult>=2) score = "Win for " + player1Name;
-        else score ="Win for " + player2Name;
+        if (minusResult == 1) score.set("Advantage " + player1Name);
+        else if (minusResult ==-1) score.set("Advantage " + player2Name);
+        else if (minusResult>=2) score.set("Win for " + player1Name);
+        else score.set("Win for " + player2Name);
         return score.toString();
     }
 
