def1752ec5e8d74c3f373232a1b2ce3a94756599
sarathip.tec <sarathip.tec@ngernturbo.com>
2021-07-24 15:48:45 +0700

    inint random

--- workshop1/restapi/src/main/java/com/example/restapi/RestapiApplication.java
+++ workshop1/restapi/src/main/java/com/example/restapi/RestapiApplication.java
@@ -3,11 +3,20 @@ package com.example.restapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Random;
 
 @SpringBootApplication
 public class RestapiApplication {
 
+	@Bean
+	public Random createRandom() {
+		return new Random();
+	}
+	
 	public static void main(String[] args) {
+
 		ConfigurableApplicationContext ctx
 				= SpringApplication.run(RestapiApplication.class, args);
 
@@ -21,3 +30,4 @@ public class RestapiApplication {
 
 
 }
+
