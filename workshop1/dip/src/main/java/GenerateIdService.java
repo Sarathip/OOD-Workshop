4b7ae8eaee95bebe7b2fcac2c1b12ea802cbe792
sarathip.tec <sarathip.tec@ngernturbo.com>
2021-07-24 14:20:35 +0700

    rest

--- workshop1/dip/src/main/java/GenerateIdService.java
+++ workshop1/dip/src/main/java/GenerateIdService.java
@@ -1,9 +1,14 @@
 import java.util.Random;
 
 public class GenerateIdService {
+    private Random random;
     public String getId() {
         Random random = new Random();
         int number = random.nextInt(10);
         return  "XYZ" + number;
+    }
+
+    public void setRandom(Random random) {
+        this.random = random;
     }
 }
