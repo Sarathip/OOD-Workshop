12bd8a311bc4a985a97ec307a9f9db9843fe9799
sarathip.tec <sarathip.tec@ngernturbo.com>
2021-07-24 15:42:01 +0700

    mockito

--- workshop1/restapi/src/main/java/com/example/restapi/GenerateIdService.java
+++ workshop1/restapi/src/main/java/com/example/restapi/GenerateIdService.java
@@ -1,13 +1,22 @@
+
 package com.example.restapi;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Random;
 
 @Component
 public class GenerateIdService {
-    public int counter;
 
-    public String getId() {
-        return "XYZ7";
+    private Random random = new Random();
+
+    @Autowired
+    public void setRandom(Random random) {
+        this.random = random;
     }
 
+    public String getId() {
+        return "XYZ" + random.nextInt(10);
+    }
 }
